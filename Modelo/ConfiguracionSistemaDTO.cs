e19dbc023be3728fe42ef74e28adf2ae9b3d22e0
Akex <alexfosters2014@gmail.com>
2021-05-23 20:41:34 -0300

    ConfiguracionesRepositorio

---
+++ Modelo/ConfiguracionSistemaDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ConfiguracionSistemaDTO
+    {
+        public int Id { get; set; }
+        public int AnticipacionVtos { get; set; }
+        //configuracion para el envio de mail
+        [MaxLength(70)]
+        public string DominioSmtp { get; set; }
+        [MaxLength(10)]
+        public int puerto { get; set; }
+        public bool Ssl { get; set; }
+        [MaxLength(100)]
+        public string correo { get; set; }
+        [MaxLength(40)]
+        public string passMail { get; set; }
+    }
+}
--- Negocio/Repositorios/ProveedorRepositorio.cs
+++ Negocio/Repositorios/ProveedorRepositorio.cs
@@ -29,7 +29,7 @@ namespace Negocio.Repositorios
                     Proveedor proveedorDB = await db.Proveedores.FindAsync(proveedorId);
                     Proveedor proveedor = mapper.Map<ProveedorDTO, Proveedor>(proveedorDTO, proveedorDB);
                     proveedor.Activo = true;
-                    var updateProveedor = db.Update(proveedor);
+                    var updateProveedor = db.Proveedores.Update(proveedor);
                     await db.SaveChangesAsync();
                     return mapper.Map<Proveedor, ProveedorDTO>(updateProveedor.Entity);
                 }
