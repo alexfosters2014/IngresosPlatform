7db73e6b2309e5284b69fc441745f27731edb6bb
Akex <alexfosters2014@gmail.com>
2021-05-24 01:21:13 -0300

    comienzo con envios de mail

---
+++ Modelo/MailDTO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MailDTO
+    {
+       
+        public string DominioSmtp { get; set; }
+     
+        public int Puerto { get; set; }
+        public bool Ssl { get; set; } = false;
+      
+        public string Correo { get; set; }
+        
+        public string PassMail { get; set; }
+
+        public bool EnvioAutentificacionProveeodr(string destinatario, string pass)
+        {
+            try
+            {
+            SmtpClient smtp = new SmtpClient(DominioSmtp, Puerto);
+            smtp.Credentials = new NetworkCredential(Correo,PassMail);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = Ssl;
+            smtp.UseDefaultCredentials = false;
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(Correo, Correo);
+            mail.To.Add(new MailAddress(destinatario));
+            mail.Subject = "Nuevo usuario de Ingresos Platform";
+            mail.IsBodyHtml = false;
+            mail.Body = $"Bienvenidos a la Ingresos Platform. Su usuario es su RUT y la contraseña inicial es:{pass}, la cual deberá cambiar una vez autentificado al sistema";
+
+            smtp.Send(mail);
+            mail.Dispose();
+
+            return true;
+            }catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+    }
+}
