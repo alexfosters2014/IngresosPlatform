653501abedd6ec374c837e6b6b1aec6e62f52505
Akex <alexfosters2014@gmail.com>
2021-06-11 21:52:35 -0300

    Arreglo de fallos Generales

--- Modelo/IngresoDTO.cs
+++ Modelo/IngresoDTO.cs
@@ -11,16 +11,21 @@ namespace Modelo
     {
         public int Id { get; set; }
         public DateTime? Fecha { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un funcionario")]
         public FuncionarioDTO Funcionario { get; set; }
-        [DataType(DataType.Time)]
+        [Required(ErrorMessage = "Debe ingresar un horario de entrada")]
+        [DataType(DataType.Time,ErrorMessage ="Debe ingresar un horario de entrada")]
         public DateTime? EntradaPlanificada { get; set; }
-        [DataType(DataType.Time)]
+        [Required(ErrorMessage = "Debe ingresar un horario de salida")]
+        [DataType(DataType.Time, ErrorMessage = "Debe ingresar un horario de salida")]
         public DateTime? SalidaPlanificada { get; set; }
         [MaxLength(30)]
         public string EstadoAutorizacion { get; set; }
         [MaxLength(150)]
         public string Comentarios { get; set; }
+        [Required(ErrorMessage ="Debe ingresar una fecha de inicio")]
         public DateTime? FechaInicio { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una fecha de fin")]
         public DateTime? FechaFin { get; set; }
         public ProveedorDTO Proveedor { get; set; }
         public string Indicador { get; set; }
